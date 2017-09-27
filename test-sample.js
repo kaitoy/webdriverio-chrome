55d89b0439bef5b7ed17501eac51dd2797ffdc77
kaitoy <kaitoy@pcap4j.org>
2017-09-27 22:32:41 +0900

    Add sample code.

--- test-sample.js
+++ test-sample.js
@@ -1,6 +1,8 @@
 describe('Sample', function() {
-    it("takes a screenshot of www.google.co.jp", function() {
-        browser.url('https://www.google.co.jp/');
+    it("takes a screenshot of www.google.com", function() {
+        browser.url('https://www.google.com/');
+        browser.waitForExist('#lst-ib');
+        browser.setValue('#lst-ib', 'yahoooooooo');
         browser.saveScreenshot('./screenshots/google.png');
     });
 });
